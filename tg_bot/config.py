414e03d6bf4ea82cdef364bc1cfc5ac1cdcb996e
nigamkumar9443-sys <nigamkumar9443@gmail.com>
2026-07-11 22:38:51 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -4,6 +4,7 @@ class Config(object):
     LOGGER = True
     API_KEY = os.environ.get("BOT_TOKEN", None)
     OWNER_ID = 804035903
+    OWNER_USERNAME = "TGNEXUSBACK"
     
     CHANNEL_LINK = "Amazon_Deal_Here"
     CHANNEL_LINK_2 = "DARKGLOBALNET"
